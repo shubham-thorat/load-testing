033a3a19a54d19e9b5a375f2410d93ad45048b45
Shubham <shubham.thorat@gameskraft.com>
2023-08-17 18:02:31 +0530

    add timing

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const logger = require('gk-logger')({
   "error_file": '/node-http2-error.log'
 });
 const RedisClient = require('./redis/redisClient')
+const helper = require('./helper')
 
 const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, '../ssl/localhost-privkey.pem')), //private key
@@ -20,27 +21,22 @@ const getTime = (startTime) => {
   return (Date.now() - startTime) / 1000;
 }
 
+let times = []
 
-let total_time = 0;
-let min_value = 0, max_value = 0;
-let index = 0;
+
 process.on('SIGINT', function () {
   console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
-  // some other closing procedures go here
-  total_time = 0;
+  // some other closing procedures go her
   process.exit(0);
 });
 
 server.on('stream', (stream, headers) => {
   // console.log("stream started.....")
   const startTime = Date.now()
-  index += 1;
-  if (min_value === 0) {
-    min_value = startTime;
-  }
+
   const method = headers[':method'];
   const path = headers[':path'];
-  const request_count = headers['count'] ?? 0;
+
   logger.info(JSON.stringify({
     "Method": method,
     "Path": path,
@@ -56,8 +52,7 @@ server.on('stream', (stream, headers) => {
       msg: 'Stream Aborted',
       streamId: stream.id,
       time: timeRequired,
-      TimeDiffServer: timeRequired,
-      requestCount: request_count
+      TimeDiffServer: timeRequired
     }))
   })
 
@@ -70,7 +65,6 @@ server.on('stream', (stream, headers) => {
       msg: `Error occured ${JSON.stringify(err)}`,
       streamId: stream.id,
       TimeDiffServer: timeRequired,
-      requestCount: request_count
     }))
   })
 
@@ -84,7 +78,6 @@ server.on('stream', (stream, headers) => {
       "Path": path,
       "streamId": stream.id,
       "TimeDiffServer": timeRequired,
-      requestCount: request_count
     }))
   } else {
     let data = ''
@@ -97,34 +90,27 @@ server.on('stream', (stream, headers) => {
       logger.info(JSON.stringify({
         streamId: stream.id,
         DataReceivedAtServer: data,
-        requestCount: request_count
       }))
 
       try {
         const payload = data === '' ? '{}' : JSON.parse(data);
+        const request_count = Number.parseInt(payload.count) ?? 0;
         RedisClient.setKey(payload.key, payload.value).then(response => {
           const endTime = Date.now();
-          total_time += (endTime - startTime);
-          max_value = Math.max(max_value, endTime)
-
-          logger.info(JSON.stringify({
-            msg: 'Redis key set success',
-            streamId: stream.id,
-            redisResponse: response,
-            requestCount: request_count,
-            endTime: endTime,
-            TimeDiffServer: (endTime - startTime) / 1000,
-            totalTime: total_time / 1000,
-            "max time - min time": (max_value - min_value) / 1000,
-            "index": index
-          }))
           stream.respond({ ':status': 200 })
+          times.push(endTime - startTime);
+          if (times.length === request_count) {
+            console.log("Total Request: ", times.length)
+            helper.calculate(times, logger)
+            times = []
+          }
+
           stream.end(JSON.stringify({
             msg: 'Redis key set success',
             streamId: stream.id,
-            requestCount: request_count,
             TimeDiffServer: (endTime - startTime) / 1000,
           }))
+
         }).catch(error => {
           const timeRequired = getTime(startTime);
           logger.error(JSON.stringify({ "Error while parsing or setting redis key": error }))
@@ -132,19 +118,19 @@ server.on('stream', (stream, headers) => {
           stream.end(JSON.stringify({
             msg: 'Redis key set failure',
             streamId: stream.id,
-            requestCount: request_count,
             TimeDiffServer: timeRequired,
           }))
+
         })
 
       } catch (error) {
+        times = []
         const timeRequired = getTime(startTime);
         logger.error(JSON.stringify({ "Error while parsing payload or setting redis key": error }))
         stream.respond({ ':status': 500 })
         stream.end(JSON.stringify({
           msg: error,
           streamId: stream.id,
-          requestCount: request_count,
           TimeDiffServer: timeRequired,
         }))
       }
@@ -158,6 +144,6 @@ server.on('stream', (stream, headers) => {
 
 const port = process.env.PORT || 6000
 server.listen(port, () => {
-  console.log("Inital Total Time", total_time)
+  console.log("inital total request", times.length)
   console.log(`Server running https://localhost:${port}`)
 })
