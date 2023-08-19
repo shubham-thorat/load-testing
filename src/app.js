24991482f512df8647470f8f9801e4e9ce883c2d
Ubuntu <ubuntu@ip-172-31-36-25.ap-south-1.compute.internal>
2023-08-19 08:38:17 +0000

    reset

---
+++ src/app.js
@@ -0,0 +1,160 @@
+const http2 = require('node:http2');
+const fs = require('node:fs');
+const path = require('path');
+const logger = require('gk-logger')({
+  "log_file": '/node-http2.log',
+  "error_file": '/node-http2-error.log'
+});
+const RedisClient = require('./redis/redisClient')
+const helper = require('./helper')
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync(path.join(__dirname, '../ssl/localhost-privkey.pem')), //private key
+  cert: fs.readFileSync(path.join(__dirname, '../ssl/localhost-cert.pem')),
+  maxSessionMemory: 1000,
+  settings: {
+    maxConcurrentStreams: 1000
+  }
+});
+
+const getTime = (startTime) => {
+  return (Date.now() - startTime) / 1000;
+}
+
+let times = []
+
+
+process.on('SIGINT', function () {
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+  // some other closing procedures go her
+  process.exit(0);
+});
+
+server.on('stream', (stream, headers) => {
+  // console.log("stream started.....")
+  const startTime = Date.now()
+
+  const method = headers[':method'];
+  const path = headers[':path'];
+  const request_count = Number.parseInt(headers['count'] ?? 0);
+
+  logger.info(JSON.stringify({
+    "Method": method,
+    "Path": path,
+    "streamId": stream.id,
+    "startTime": startTime,
+  }));
+
+
+  stream.on('aborted', () => {
+    const timeRequired = getTime(startTime);
+    console.log("server aborted")
+    logger.error(JSON.stringify({
+      msg: 'Stream Aborted',
+      streamId: stream.id,
+      time: timeRequired,
+      TimeDiffServer: timeRequired
+    }))
+  })
+
+  stream.on('error', (err) => {
+
+    const timeRequired = getTime(startTime);
+    times.set(`${stream.id}_${startTime}`, timeRequired);
+    console.log("server error occurred", JSON.stringify(err))
+    logger.error(JSON.stringify({
+      msg: `Error occured ${JSON.stringify(err)}`,
+      streamId: stream.id,
+      TimeDiffServer: timeRequired,
+    }))
+  })
+
+
+  if (method === 'GET') {
+    logger.info(`GET method request received at server for streamId : ${stream.id}`)
+    stream.respond({ ':status': 200 });
+    const timeRequired = getTime(startTime);
+    stream.end(JSON.stringify({
+      "Method": method,
+      "Path": path,
+      "streamId": stream.id,
+      "TimeDiffServer": timeRequired,
+    }))
+  } else {
+    let data = ''
+    stream.setEncoding('utf-8')
+    stream.on('data', (chunk) => {
+      data += chunk
+    })
+
+    stream.on('end', () => {
+      logger.info(JSON.stringify({
+        streamId: stream.id,
+        DataReceivedAtServer: data,
+      }))
+
+      try {
+        const payload = data === '' ? '{}' : JSON.parse(data);
+
+        RedisClient.setKey(payload.key, payload.value).then(response => {
+          const endTime = Date.now();
+          stream.respond({ ':status': 200 })
+          times.push(endTime - startTime);
+
+          logger.info(JSON.stringify({
+            msg: 'Redis key set success',
+            streamId: stream.id,
+            TimeDiffServer: (endTime - startTime) / 1000,
+            "request count": times.length
+          }))
+
+          if (times.length === request_count) {
+            console.log("Total Request: ", times.length)
+            helper.calculate(times, logger)
+            times = []
+            console.log("Array length after processing", times.length)
+          }
+
+          stream.end(JSON.stringify({
+            msg: 'Redis key set success',
+            streamId: stream.id,
+            TimeDiffServer: (endTime - startTime) / 1000,
+            "request count": times.length
+          }))
+
+        }).catch(error => {
+          const timeRequired = getTime(startTime);
+          logger.error(JSON.stringify({ "Error while parsing or setting redis key": error }))
+          stream.respond({ ':status': 500 })
+          stream.end(JSON.stringify({
+            msg: 'Redis key set failure',
+            streamId: stream.id,
+            TimeDiffServer: timeRequired,
+          }))
+
+        })
+
+      } catch (error) {
+        times = []
+        const timeRequired = getTime(startTime);
+        logger.error(JSON.stringify({ "Error while parsing payload or setting redis key": error }))
+        stream.respond({ ':status': 500 })
+        stream.end(JSON.stringify({
+          msg: error,
+          streamId: stream.id,
+          TimeDiffServer: timeRequired,
+        }))
+      }
+    })
+
+  }
+})
+
+
+
+
+const port = process.env.PORT || 6000
+server.listen(port, () => {
+  console.log("inital total request", times.length)
+  console.log(`Server running https://localhost:${port}`)
+})
---
+++ src/helper.js
@@ -0,0 +1,70 @@
+function calculatePercentile(array, percentile) {
+  if (array.length === 0) {
+    console.log("Array is empty.");
+    return { result: 0, min_value: 0, max_value: 0 }
+  }
+
+  array.sort((a, b) => a - b);
+  const min_value = array.length != 0 ? array[0] : 0;
+  const max_value = array.length != 0 ? array[array.length - 1] : 0;
+
+  const index = (percentile / 100) * (array.length - 1);
+  if (Number.isInteger(index)) {
+    return { result: array[index], min_value, max_value };
+  } else {
+    const lowerIndex = Math.floor(index);
+    const upperIndex = Math.ceil(index);
+    const lowerValue = array[lowerIndex];
+    const upperValue = array[upperIndex];
+    const fraction = index - lowerIndex;
+    let result = (upperValue - lowerValue) * fraction + lowerValue;
+
+    return { result: result.toFixed(2), min_value, max_value }
+  }
+}
+
+
+function calculateMean(array) {
+  if (array.length === 0) {
+    throw new Error("Array is empty.");
+  }
+
+  const sum = array.reduce((total, num) => total + num, 0);
+  const mean = sum / array.length;
+  return mean;
+}
+
+const calculate = (times, logger) => {
+  const startTime = Date.now();
+  console.log("startTime", startTime)
+  const r1 = calculatePercentile(times, 99);
+  const per_50 = calculateMean(times);
+  const r2 = calculatePercentile(times, 50);
+  const endTime = Date.now();
+  console.log("endtime", endTime)
+
+  logger.info(JSON.stringify({
+    "99th Percentile": `${r1.result}ms`,
+    "50 Percentile": `${r2.result}ms`,
+    "min time required for request": `${r1.min_value}ms`,
+    "max time required for request": `${r1.max_value}ms`,
+    "average": `${per_50.toFixed(3)}ms`,
+    "proccessing time": `${(endTime - startTime)}ms`,
+  }))
+
+
+  console.log({
+    "99th Percentile": `${r1.result}ms`,
+    "50 Percentile": `${r2.result}ms`,
+    "min time required for request": `${r1.min_value}ms`,
+    "max time required for request": `${r1.max_value}ms`,
+    "average": `${per_50.toFixed(3)}ms`,
+    "proccessing time": `${(endTime - startTime)}ms`,
+  })
+}
+
+module.exports = {
+  calculateMean,
+  calculatePercentile,
+  calculate
+}
